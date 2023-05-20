7e6e3f5537a70250188d99c28c0e4b5b25f82499
AdnanIqbalKhan <hafizadnaniqbalkhan@gmail.com>
2023-05-20 11:14:30 +0500

    fix

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -9,9 +9,9 @@ export const getURL = () => {
   url = url.includes('http') ? url : `https://${url}`;
   // Make sure to including trailing `/`.
   url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
-  alert("1111",url)
-  aler("222",process.env.NEXT_PUBLIC_SITE_URL)
-alertt("333",process.env.NEXT_PUBLIC_VERCEL_URL)
+  alert(url)
+  alert(process.env.NEXT_PUBLIC_SITE_URL)
+  alert(process.env.NEXT_PUBLIC_VERCEL_URL)
   return url;
 };
 
